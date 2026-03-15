75f9de861939e5ba75f4ad05b7ee658c9a19482a
Lucas IQ Nordic <iqnordic@iqnordic.dk>
2026-03-15 19:11:01 +0100

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Lucas Kildegaard on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
